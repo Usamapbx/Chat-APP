8ab94243ae429b393d73d2d3a513b835d7745d24
usamapbx <usamapbx1@gmail.com>
2024-07-30 23:28:36 +0500

    project added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign up</title>
+    <link rel="stylesheet" href=" style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form-signup">
+            <header>Chat-APP</header>
+            <form action="#">
+                <div class="error txt">This is an error message</div>
+                <div class="name details">
+                    <div class="field">
+                        <label>First Name</label>
+                        <input type="text" placeholder="First Name">
+                    </div>
+                    <div class="field">
+                        <label>Last Name</label>
+                        <input type="text" placeholder="Last Name">
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Email</label>
+                    <input type="text" placeholder="Enter Your Email">
+                </div><div class="field">
+                    <label>Password</label>
+                    <input type="text" placeholder="Enter Your Password">
+                </div><div class="field">
+                    <label>Select Picture</label>
+                    <input type="file" >
+                </div>
+                <div class="field">
+                    <input type="submit" value="Continue">
+                </div>
+            </form>
+            <dive class="link">Already have an account? <a href="">Login</a></dive>
+        </section>
+    </div>
+</body>
+</html>
